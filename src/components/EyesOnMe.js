92a3b9e039ccb1678fed31bb12fc6b982add9746
Farah Arar <farah02arar@gmail.com>
2023-10-23 07:49:25 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe(){
+    function focus(){
+        console.log('Good!');
+    }
+    function blur(){
+        console.log('Hey! Eyes on me!');
+    }
+    return(
+        <div>
+            <button onFocus={focus} onBlur={blur}></button>
+            
+        </div>
+    )
+}
+export default EyesOnMe;
